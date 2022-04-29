0e91e8799d693264e561f92075c200bd74e2d664
Shawndek <sdek@gmx.de>
2022-04-29 16:39:50 +0200

    images controller fixed

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
 import express  from "express";
-import itmesRouter from "./src/routes/itemsRouter.js";
-//import imagesRouter from "./src/routes/itemsRouter.js";
+import imagesRouter from "./src/routes/imagesRouter.js";
+import itemsRouter from "./src/routes/itemsRouter.js";
 import cors from "cors";
-import imageUploader from "./src/middlewares/imageUploader.js";
-import { uploadFile } from "./s3.js"  
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors({origin: "*"}));
 app.use(express.json());
-app.use('/items', itmesRouter);
-//app.use('/upload-pic', imagesRouter);
+app.use('/items', itemsRouter);
+app.use('/upload-pic', imagesRouter);
 app.get('/', (req,res) => {
     res.send('Items API')
 });
- app.post('/upload-pic', imageUploader.single('pic1'), async(req, res, next) => {
-  if (!req.file) throw new Error('Please upload an image');
-  //console.log(req.file)
-  const result = await uploadFile(req.file)
-  //console.log(result)
-  console.log(req.file.filename)
-  res.json({result})
-});  
 
 app.listen(port, () => console.log(`server runnig at localhost ${port}`));
--- src/controllers/images.js
+++ src/controllers/images.js
@@ -1,10 +1,14 @@
-import { uploadFile } from "./s3.js"
+import { uploadFile } from '../../s3.js'
 
-export const postImages = (async(req, res, next) => {
-  if (!req.file) throw new Error('Please upload an image');
-  //console.log(req.file)
-  const result = await uploadFile(req.file)
-  //console.log(result)
-  console.log(req.file.filename)
-  res.json({result})
-});
+export const postImages = async (req, res, next) => {
+  try {
+    if (!req.file) throw new Error('Please upload an image');
+    //console.log(req.file)
+    const result = await uploadFile(req.file);
+    //console.log(result)
+    console.log(req.file.filename);
+    res.json({ result });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
